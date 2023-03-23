2c2f46ce3c6888ab4ecebeb6bf2d222e866abe27
William Jelgren <william@jelgren.dk>
2023-03-23 14:43:56 +0100

    Bad reconnect implemented. Could be better

--- MRW-motor/src/main.cpp
+++ MRW-motor/src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 #include <Stepper.h>
+
+#include <BLEDevice.h>
+#include <BLEClient.h>
+#include <BLEScan.h>
+
 #include <Wifi.h>
 
 #define IN1 16
@@ -10,16 +15,106 @@
 #define stepsPerRev 2038
 #define stepSpeed 15
 
-#define wifi_ssid "Wills Spot"
-#define wifi_password "Jelgrens"
+Stepper stepper = Stepper(stepsPerRev, IN1, IN3, IN2, IN4);
 
 const int half = stepsPerRev / 2;
 const int seventh = stepsPerRev / 7;
 
-Stepper stepper = Stepper(stepsPerRev, IN1, IN3, IN2, IN4);
+#define wifi_ssid "Wills Spot"
+#define wifi_password "Jelgrens"
 
-void bleSetup() {
+#define BLE_SERVER_NAME "MRW Remote"
 
+static BLEUUID serviceUUID("6f9f35df-adc2-44e1-8c02-1dcb67d42551");
+static BLEUUID inputCharacteristicUUID("a2779120-40d8-4920-8347-57655f37f23a");
+
+static BLEAddress *pServerAddress;
+
+static BLERemoteCharacteristic* inputCharacteristic;
+
+bool connectFlag = false;
+bool connected = false;
+bool scan = false;
+BLEAdvertisedDevice *connectedDevice;
+
+void inputCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+    //TODO: Write callbacks to all events.
+
+    stepper.step(half);
+}
+
+class ClientCallbacks : public BLEClientCallbacks {
+    void onConnect(BLEClient* pclient) {
+        connected = true;
+    }
+
+    void onDisconnect(BLEClient* pclient) {
+        connected = false;
+        connectFlag = true;
+        Serial.println("onDisconnect");
+    }
+};
+
+class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+        if (!advertisedDevice.haveServiceUUID()) {
+            return;
+        }
+
+        Serial.print("BLE Advertised Device with service UUID has been found: ");
+        Serial.println(advertisedDevice.toString().c_str());
+        
+        if (advertisedDevice.isAdvertisingService(serviceUUID)) {
+            BLEDevice::getScan()->stop();
+            connectedDevice = new BLEAdvertisedDevice(advertisedDevice);
+            connectFlag = true;
+            Serial.println("Found valid BLE server.");
+        }
+    }
+};
+
+void setupBLE() {
+    BLEDevice::init("");
+
+    BLEScan* pBLEScan = BLEDevice::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+    pBLEScan->setInterval(1349);
+    pBLEScan->setWindow(449);
+    pBLEScan->setActiveScan(true);
+    pBLEScan->start(3, false);
+}
+
+bool connectBLE(BLEAddress pAddress) {
+    BLEClient* pClient = BLEDevice::createClient();
+
+    pClient->setClientCallbacks(new ClientCallbacks());
+
+    pClient->connect(pAddress);
+    Serial.println("Connected to server.");
+
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+        Serial.print("Failed to find service with the given UUID: ");
+        Serial.println(serviceUUID.toString().c_str());
+        pClient->disconnect();
+
+        return false;
+    }
+
+    inputCharacteristic = pRemoteService->getCharacteristic(inputCharacteristicUUID);
+
+    if (inputCharacteristic == nullptr) {
+        Serial.print("Failed to find characteristic with the given UUID: ");
+        Serial.println(inputCharacteristicUUID.toString().c_str());
+        pClient->disconnect();
+
+        return false;
+    }
+
+    inputCharacteristic->registerForNotify(inputCallback);
+
+    Serial.println("Successfully got all characteristics.");
+    return true;
 }
 
 void connectWifi() {
@@ -40,14 +135,17 @@ void connectWifi() {
 void setup() {
     Serial.begin(115200);
 
-    connectWifi();
+    setupBLE();
+    // connectWifi();
 
     stepper.setSpeed(stepSpeed);
 }
 
 void loop() {
-    stepper.step(half);
-    delay(250);
+    if (connectFlag && !connected) {
+        connectBLE(connectedDevice->getAddress());
+        connectFlag = false;
+    }
+
+    delay(1000);
 }
-
-
