c0b3fe95f5781bbd36c07b2bd91938b323a2edd5
William Jelgren <william@jelgren.dk>
2023-03-23 12:59:21 +0100

    Remote BLE server is setup.

--- MRW-remote/src/main.cpp
+++ MRW-remote/src/main.cpp
@@ -1,21 +1,37 @@
 #include <Arduino.h>
-#include <ArduinoBle
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
 
-#define BLE_SERVER_NAME "MRW Remote 1"
+#define BLE_SERVER_NAME "MRW Remote"
 
 #define SERVICE_UUID "6f9f35df-adc2-44e1-8c02-1dcb67d42551"
 #define CHARACTERISTIC_UUID "a2779120-40d8-4920-8347-57655f37f23a"
 
+BLECharacteristic inputCharacteristic();
+BLEDescriptor inputDescriptor(BLEUUID((uint16_t)0x2902));
+
 void bleSetup() {
     Serial.println("Starting BLE server...");
+
     BLEDevice::init(BLE_SERVER_NAME);
 
     BLEServer *pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                            CHARACTERISTIC_UUID,
+                                            BLECharacteristic::PROPERTY_READ
+                                        );
+
+    pCharacteristic->setValue("start");
+
+    pService->start();
+
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->start();
+
+    Serial.println("Successfully started BLE server!");
 }
 
 void setup() {
