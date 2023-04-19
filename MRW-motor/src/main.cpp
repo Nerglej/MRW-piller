d9961273e518cbe52ca6bf1ee76aafd9a167930e
William Jelgren <william@jelgren.dk>
2023-04-19 10:28:37 +0200

    Added comments to code and fixed WiFi connection, where it would say it connected, without actually connecting.

--- MRW-motor/src/main.cpp
+++ MRW-motor/src/main.cpp
@@ -45,16 +45,21 @@ remote_control_message remote_message;
 bool clicking = false;
 bool turn = false;
 
-// Callback when data is sent
+// Callback når data bliver sendt. Udelukkende for fejlfinding.
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
     Serial.print("\r\nLast Packet Send Status:\t");
     Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-// Callback when data is received
+// Callback når data bliver modtaget
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+    // Kopiér data der bliver modtaget, til en lokal variabel, så vi kan behandle det som forventet.
     memcpy(&remote_message, incomingData, sizeof(remote_message));
 
+    // Behandl dataene, så det faktisk kan bruges i koden.
+    // Hvis den modtagene input er "released", skal den ikke gøre andet noget.
+    // Hvis den derimod er alt andet, og der ikke bliver holdt en knap nede,
+    // skal den gøre forskellige ting, som at meddele at motoren skal dreje næste loop.
     if (remote_message.input == "released") {
         clicking = false;
     }
@@ -66,43 +71,50 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
     }
 }
 
+// Opsætning af LCD
 void LCDSetup() {
     Serial.print("Setting up LCD...");
 
+    // Beskriv LCD display på 16 "kolonner" og 2 linjer/rækker.
     lcd.begin(16, 2);
     lcd.setCursor(3, 0);
+
     lcd.print("MRW-piller");
-    lcd.blink();
 }
 
+// Opsætning af ESPNOW
 void ESPNOWSetup() {
     if (esp_now_init() != ESP_OK) {
         Serial.println("Error initializing ESP-NOW");
         return;
     }
 
+    // Registrer callback når data bliver sendt
     esp_now_register_send_cb(OnDataSent);
 
-    // Register peer
+    // Registrer den forbundede peer
     memcpy(peerInfo.peer_addr, peer_address, 6);
     peerInfo.channel = 0;  
     peerInfo.encrypt = false;
     
-    // Add peer        
+    // Tilføj peer   
     if (esp_now_add_peer(&peerInfo) != ESP_OK){
         Serial.println("Failed to add peer");
         return;
     }
-    // Register for a callback function that will be called when data is received
+
+    // Registrer callback når data bliver modtaget
     esp_now_register_recv_cb(OnDataRecv);
 }
 
+// Opsætning af WiFi
 void WifiSetup() {
+    // Login til WiFi-nætværk, der svarer til værdierne.
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("\nConnecting to wifi");
 
-    // Wait until connected
-    while (WiFi.status() == WL_CONNECTED) {
+    // Vent indtil at koden er forbundet.
+    while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
         delay(500);
     }
@@ -110,6 +122,7 @@ void WifiSetup() {
     Serial.println("\nSuccessfully connected to wifi");
 }
 
+// Setup funktion der kører én gang når ESP'en starter.
 void setup() {
     Serial.begin(115200);
 
@@ -118,42 +131,51 @@ void setup() {
     // Opsætning af lyd-output
     pinMode(SOUND_PIN, OUTPUT);
 
+    // Sæt WiFi-modulet til at fungere både som access point og station.
+    // Dette gør at ESP-NOW OG POST-requests virker.
     WiFi.mode(WIFI_AP_STA);
     // Opsætning af ESPNOW
     ESPNOWSetup();
     // Opsætning af Wifi
     WifiSetup();
 
+    // Sætter hastigheden på steppermotoren.
     stepper.setSpeed(stepSpeed);
 }
 
+// Send besked til "serveren", altså remoten, men kun hvis den er forbundet til internettet.
 void sendToServer() {
     if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-    
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, SERVER_IP);
-    
-      // If you need an HTTP request with a content type: application/json, use the following:
-      http.addHeader("Content-Type", "application/json");
-
-      String data = "{\"id\":\"" DATA_USER_ID "\",\"serial\":\"" DATA_SERIAL "\",\"data\":{\"pulse\": 70}}";
-      int httpResponseCode = http.POST(data);
-     
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+        WiFiClient client;
+        HTTPClient http;
         
-      // Free resources
-      http.end();
+        // Server_ip'en bliver indtastet
+        http.begin(client, SERVER_IP);
+        
+        // Beskriv at vi sender JSON til serveren, så serveren ved hvordan det skal behandles.
+        http.addHeader("Content-Type", "application/json");
+
+        // JSON-dataene som en rå string, der bliver sendt.
+        String data = "{\"id\":\"" DATA_USER_ID "\",\"serial\":\"" DATA_SERIAL "\",\"data\":{\"pulse\": 70}}";
+        // Faktisk send dataene, og modtag en status-kode. Burde gerne være 200, OK
+        int httpResponseCode = http.POST(data);
+        
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+            
+        // Stop forbindelsen, så den ikke ligger og fylder i hukommelsen.
+        http.end();
     }
     else {
-      Serial.println("Offline");
+        Serial.println("Offline");
     }
 }
 
+// Kører hele tiden
 void loop() {
+    // Hvis motoren skal dreje
     if (turn) {
+        // Skriv til LCD at den er ved at gøre noget, samtidig med at en lyd spiller.
         lcd.setCursor(1, 1);
         lcd.write("Pilling you...");
 
@@ -167,6 +189,7 @@ void loop() {
         lcd.write("              ");
         turn = false;
 
+        // Send besked til server for at logge det.
         sendToServer();
     }
 
